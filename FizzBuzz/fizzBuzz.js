13e504d79fb17b56270359117e13b410f0d017d6
Jennifer Colna <jennifer.colna@gmail.com>
2018-06-14 13:31:18 -0500

    fizz buzz fix

--- FizzBuzz/fizzBuzz.js
+++ FizzBuzz/fizzBuzz.js
@@ -1,28 +1,32 @@
 'use strict';
+function main(){
 //when submit button is clicked...
-$('main').on('click','button', function(event){
-//find the value of the input (number to count to)
-  let count = $('#number-choice').val();
-  //create an array of all of the numbers between 0-input number
-  for(let i=1;i<=count;i++){
-  //loop through the array to check conditions
-    if (i%15===0){
-    //add <div class="fizz-buzz-item"><span>1</span></div> with content and class
-    //specific to number (number, fizz, buzz, or fizzbuzz) to .js-results div
-      $('.js-results').append(
-        '<div class="fizz-buzz-item fizzbuzz"><span>fizzbuzz</span></div>');
+  $('form').on('submit', function(event){
+    event.preventDefault();
+    //find the value of the input (number to count to)
+    let count = $('#number-choice').val();
+    //create an array of all of the numbers between 0-input number
+    for(let i=1;i<=count;i++){
+    //loop through the array to check conditions
+      if (i%15===0){
+      //add <div class="fizz-buzz-item"><span>1</span></div> with content and class
+      //specific to number (number, fizz, buzz, or fizzbuzz) to .js-results div
+        $('.js-results').append(
+          '<div class="fizz-buzz-item fizzbuzz"><span>fizzbuzz</span></div>');
+      }
+      else if (i%3===0){
+        $('.js-results').append(
+          '<div class="fizz-buzz-item fizz"><span>fizz</span></div>');
+      }
+      else if (i%5===0){  
+        $('.js-results').append(
+          '<div class="fizz-buzz-item buzz"><span>buzz</span></div>');
+      }
+      else {
+        $('.js-results').append(
+          `<div class="fizz-buzz-item"><span>${i}</span></div>`);
+      }
     }
-    else if (i%3===0){
-      $('.js-results').append(
-        '<div class="fizz-buzz-item fizz"><span>fizz</span></div>');
-    }
-    else if (i%5===0){  
-      $('.js-results').append(
-        '<div class="fizz-buzz-item buzz"><span>buzz</span></div>');
-    }
-    else {
-      $('.js-results').append(
-        `<div class="fizz-buzz-item"><span>${i}</span></div>`);
-  }
-  }
-});
+  });
+}
+$(main);
