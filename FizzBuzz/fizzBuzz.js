c0f3ff7c3d5f3ffd89dae978d58f57fce6877b72
Jennifer Colna <jennifer.colna@gmail.com>
2018-06-14 12:51:15 -0500

    fizz buzz drills

---
+++ FizzBuzz/fizzBuzz.js
@@ -0,0 +1,28 @@
+'use strict';
+//when submit button is clicked...
+$('main').on('click','button', function(event){
+//find the value of the input (number to count to)
+  let count = $('#number-choice').val();
+  //create an array of all of the numbers between 0-input number
+  for(let i=1;i<=count;i++){
+  //loop through the array to check conditions
+    if (i%15===0){
+    //add <div class="fizz-buzz-item"><span>1</span></div> with content and class
+    //specific to number (number, fizz, buzz, or fizzbuzz) to .js-results div
+      $('.js-results').append(
+        '<div class="fizz-buzz-item fizzbuzz"><span>fizzbuzz</span></div>');
+    }
+    else if (i%3===0){
+      $('.js-results').append(
+        '<div class="fizz-buzz-item fizz"><span>fizz</span></div>');
+    }
+    else if (i%5===0){  
+      $('.js-results').append(
+        '<div class="fizz-buzz-item buzz"><span>buzz</span></div>');
+    }
+    else {
+      $('.js-results').append(
+        `<div class="fizz-buzz-item"><span>${i}</span></div>`);
+  }
+  }
+});
